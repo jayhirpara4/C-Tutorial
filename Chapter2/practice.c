3fd3ee5b297bb0b7869602d617083d033fd4bacd
Jay Hirpara <jayhirpara948@gmail.com>
2023-07-28 22:27:37 +0530

    next

--- Chapter2/practice.c
+++ Chapter2/practice.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
         int main() {
+
+            //even -> 1
+            //odd -> 0
   
             int x;
             printf("enter a number : ");
             scanf("%d", &x);
-            printf("%d", x % 2 == 0);
+            printf("%d \n", x % 2 == 0);
+
+            int n;
+            printf("Write the number : ");
+            scanf("%d" , &n);
+            printf("Your answer : %d", 9<n && n<100 );
 
         return 0;
 }
