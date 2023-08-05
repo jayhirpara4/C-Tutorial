051609db52e3d97cc785a167776086a3536de4cf
Jay Hirpara <jayhirpara948@gmail.com>
2023-08-05 18:50:50 +0530

    Next

--- Chapter2/Logical-Operator.c
+++ Chapter2/Logical-Operator.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
     int main() {
-    printf("%d \n", 3<4 && 3<5);
+    printf("%d \n", (3<4 && 3<5));
     printf("%d \n", 3<4 && 5<4);
     printf("%d \n", 3<4 && 5<4);
     printf("%d \n", 3>4 && 5>4);
